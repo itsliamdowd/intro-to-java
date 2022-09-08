513cdf5c6be5ad1ba5e0a0b892206bd2f84d0842
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-09-07 23:37:55 -0700

    Create tree.java

---
+++ tree.java
@@ -0,0 +1,16 @@
+//A program to display a tree in java
+
+public class Tree {
+    public static void main(String[] args) {
+      System.out.println("       *");
+      System.out.println("      ***");
+      System.out.println("     *****");
+      System.out.println("    *******");
+      System.out.println("   *********");
+      System.out.println("  ***********");
+      System.out.println(" *************");
+      System.out.println("***************");
+      System.out.println("      ***");
+      System.out.println("      ***");
+    }
+  }
